425e24d3ee0deeb79d63a42ae44cef2c7c2efe18
Artem <artemtsvelov@gmail.com>
2020-10-01 13:24:30 +0300

    add code

--- js/main-2.js
+++ js/main-2.js
@@ -4,6 +4,7 @@ let box = document.querySelector('.box');
 let contrl = document.querySelector('input[type=checkbox]');
 let paravozik = document.getElementById('paravozik');
 let temp = 0;
+let timerId
 
 contrl.addEventListener('click', (event) => {
     if (event.target.type === 'checkbox') {
@@ -11,13 +12,25 @@ contrl.addEventListener('click', (event) => {
     };
   });
 btnLeft.addEventListener('mousedown', () => {
-    temp = temp - 2;
-    paravozik.style.left = temp + 'px';
+    timerId = setInterval(() => {
+        temp = temp - 1;
+        paravozik.style.left = temp + 'px';
+    });
 });
 
+btnLeft.addEventListener('mouseup', () => {
+    clearInterval(timerId)
+});
+    
 btnRight.addEventListener('mousedown', () => {
-    temp = temp + 2;
-    paravozik.style.left = temp + 'px';
+    timerId = setInterval(() => {
+        temp = temp + 1;
+        paravozik.style.left = temp + 'px';
+    });
+});
+
+btnRight.addEventListener('mouseup', () => {
+    clearInterval(timerId)
 });
 
 document.addEventListener('keydown', (event) => {
