b50b1589ae8fbbf21d0d8df854192b6ca8634aea
Artem <artemtsvelov@gmail.com>
2020-10-10 12:49:29 +0300

    code

--- js/main-1.js
+++ js/main-1.js
@@ -12,8 +12,8 @@ const doubleMas = () => {
     }
 }
 
-
 // 2) напиши функцию очистки массива, заполнить null
+
 const clearMas = mas.map((num) => {
     num = null;
     return num
@@ -33,8 +33,8 @@ body.prepend(strStatus);
 strStatus.innerHTML = `Количество ненулевых клеток ${findMas}`;
 
 // 4) отобрази на странице игровое поле(клетки массива)
+let mainBlock = document.querySelector('.main');
 const createBlock = () => {
-    let mainBlock = document.querySelector('.main');
     let block;
     for (let i = 0; i < 5; i++) {
         for (let j = 0; j < 5; j++) {
@@ -48,12 +48,11 @@ createBlock();
 
 // 6) сделай управление мышкой и клавиатурой, там где находится курсор
 const box = document.querySelectorAll('.box');
-
 const findCoursor = box.forEach((i) => {
     i.addEventListener('mouseover', (event) => {
         event.target.classList.add('color');
         event.relatedTarget.classList.remove('color');
-    }); 
+    });
 });
 
 
