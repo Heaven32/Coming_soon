7b80c51e2a05aeefaa8e1f57ccc95636a22aa8da
Artem <artemtsvelov@gmail.com>
2020-10-19 20:24:14 +0300

    add code

--- js/main.js
+++ js/main.js
@@ -1,36 +1,23 @@
-const cartEl = document.querySelector('.cart');
+const cardEl = document.querySelector('.card');
+const balanceCards =document.querySelector('.balance_cards');
 const gameBoard = document.querySelector('.game-board');
+
 const count = 10;
-const cart = [];
+const card = [];
+
+// Обьект с типом карт
+const cardTypes  = [ 
+    {name: "CardOne", type: 1, adress: `<img src="./image/corner.png" alt="">`},
+    {name: "CardTwo", type: 2, adress: `<img src="./image/impasse.png" alt="">`},
+    {name: "CardThree", type: 3, adress: `<img src="./image/stick.png" alt="">`},
+];
 
 // массив со случайным набором чисел
-const arrCart = [];
-const quantityCart = 30;
-for (let i = 0; i < quantityCart; i++) {
-    arrCart.push(Math.round(Math.random() * quantityCart))
+const arrCard = [];
+const quantityCard = 30;
+for (let i = 0; i < quantityCard; i++) {
+    arrCard.push(cardTypes[Math.floor(Math.random() * Math.floor(3))]);
 }
-console.log(arrCart)
-
-// переменные с типом карт
-const CartOne = `<div class='corner'>
-                    <img src="./image/corner.png" alt="">
-                </div>`;
-const CartTwo = `<div class='impasse'>
-                    <img src="./image/impasse.png" alt="">
-                </div>`;
-const CartThree = `<div class='stick'>
-                        <img src="./image/stick.png" alt="">
-                </div>`;
-
-// Сгенерировать 30 карт 
-const generateCart = (cartArr) => {
-    for(let i = 1; i <= count; i++) {
-        cartEl.innerHTML
-    };
-    return cartArr;
-};
-generateCart(cart);
-console.log(generateCart(cart))
 
 // Отрисовка игрового поля
 let fieldsNumb = 8;
@@ -48,8 +35,36 @@ doubleMas();
 const createBlock = () => {
     arr.forEach(i => {
         i.forEach(x => {
-            gameBoard.innerHTML += `<div class='box'/>`  
+            gameBoard.innerHTML += `<div class='box'> </div>`  
             });       
-    });
+    }); 
 };
 createBlock();
+
+// колличество оставшихся карт в колоде
+balanceCards.innerHTML = `Осталось ${arrCard.length} карт`
+
+// Отрисовка одной карты из всей колоды
+cardEl.innerHTML = arrCard[0].adress;
+
+// переменная для сохранения угла градуса картинки
+let angle = 0;
+// Клик по игровому полю
+const boxAll = document.querySelectorAll('.box');
+function clickBoardGame () { 
+    cardEl.innerHTML = arrCard[1].adress;
+    if (this.textContent === ' '){
+        this.innerHTML = arrCard[0].adress;
+        arrCard.shift()
+    }else {
+        angle += 90;
+        this.style.transform = `rotate(${angle}deg)`
+    }
+    balanceCards.innerHTML = `Осталось ${arrCard.length} карт`
+};
+
+const addListeners = () => {
+    boxAll.forEach(elem => elem.addEventListener('click', clickBoardGame))
+   
+};
+addListeners();
