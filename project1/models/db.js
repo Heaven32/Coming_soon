9fb92c8ec449df7fa47e986f10ca00cff634d4e7
Artem <artemtsvelov@gmail.com>
2020-11-14 00:48:36 +0200

    pagination

---
+++ project1/models/db.js
@@ -0,0 +1,13 @@
+const text = [
+    '1Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos doloremque nulla sapiente laboriosam error obcaecati illum amet. Minima obcaecati vitae optio, ab veritatis exercitationem voluptates, quaerat aspernatur rerum perferendis provident.',
+    '2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos doloremque nulla sapiente laboriosam error obcaecati illum amet. Minima obcaecati vitae optio, ab veritatis exercitationem voluptates, quaerat aspernatur rerum perferendis provident.',
+    '3Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos doloremque nulla sapiente laboriosam error obcaecati illum amet. Minima obcaecati vitae optio, ab veritatis exercitationem voluptates, quaerat aspernatur rerum perferendis provident.',
+    '4Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos doloremque nulla sapiente laboriosam error obcaecati illum amet. Minima obcaecati vitae optio, ab veritatis exercitationem voluptates, quaerat aspernatur rerum perferendis provident.',
+    '5Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos doloremque nulla sapiente laboriosam error obcaecati illum amet. Minima obcaecati vitae optio, ab veritatis exercitationem voluptates, quaerat aspernatur rerum perferendis provident.',
+    '6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos doloremque nulla sapiente laboriosam error obcaecati illum amet. Minima obcaecati vitae optio, ab veritatis exercitationem voluptates, quaerat aspernatur rerum perferendis provident.',
+    '7Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos doloremque nulla sapiente laboriosam error obcaecati illum amet. Minima obcaecati vitae optio, ab veritatis exercitationem voluptates, quaerat aspernatur rerum perferendis provident.',
+    '8Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos doloremque nulla sapiente laboriosam error obcaecati illum amet. Minima obcaecati vitae optio, ab veritatis exercitationem voluptates, quaerat aspernatur rerum perferendis provident.',
+    '9Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos doloremque nulla sapiente laboriosam error obcaecati illum amet. Minima obcaecati vitae optio, ab veritatis exercitationem voluptates, quaerat aspernatur rerum perferendis provident.'
+];
+
+module.exports = text;
