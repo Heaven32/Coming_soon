eb0ab5c212939207cba3affabe10076e592aacff
Artem <artemtsvelov@gmail.com>
2021-05-04 20:33:10 +0300

    add axios get (test page)

--- src/Components/Users/index.js
+++ src/Components/Users/index.js
@@ -2,70 +2,22 @@ import React from 'react';
 import classes from './style.module.css'
 import {connect} from "react-redux";
 import {followAC, setUsersAC, unfollowAC} from "../../Redux/users-reducer";
+import userPhoto from '../../assets/images/users.png'
 import axios from "axios";
 
 const UsersFunction = (props) => {
+
     if (props.users.length === 0) {
-
-        axios.get('https://social-network.samuraijs.com/api/')
-
-        props.setUsers( [
-            {
-                id: 1,
-                followed: true,
-                status: 'I`m boss',
-                location: {
-                    city: 'Dnipro',
-                    country: 'Ukraine'
-                },
-                message: 'The message on the card said \'Be my Valentine\'.',
-                name: 'User1',
-                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYs5pjJL91GmWCY320ehmbGjILJt5ofZCmxA&usqp=CAU'
-            },
-            {
-                id: 2,
-                followed: false,
-                status: 'I`m boss1',
-                location: {
-                    city: 'Dnipro1',
-                    country: 'Ukraine'
-                },
-                message: 'She wasn\'t in so I left a message on her answerphone',
-                name: 'User2',
-                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6_yv0U_2N_0KokOGogPD0al8ZsxrOK4Hu3A&usqp=CAU'
-            },
-            {
-                id: 3,
-                followed: true,
-                status: 'I`m boss2',
-                location: {
-                    city: 'Dnipro2',
-                    country: 'Ukraine'
-                },
-                message: 'Can you give Jo a message for me? Tell her I\'ve booked a tennis court for 6.30.',
-                name: 'User3',
-                avatar: 'https://chto-takoe-lyubov.net/wp-content/uploads/2020/05/Risunki-dlya-srisovki-karandashom-Smeshariki-1.jpg'
-            },
-            {
-                id: 4,
-                followed: false,
-                status: 'I`m boss3',
-                location: {
-                    city: 'Dnipro3',
-                    country: 'Ukraine'
-                },
-                message: 'When I got home, I found a mysterious message sellotaped to the front door',
-                name: 'User4',
-                avatar: 'https://klike.net/uploads/posts/2018-06/1528718816_2.jpg'
-            },
-        ])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        })
     }
 
     return <div>
         {props.users.map(u => <div key={u.id}>
             <span>
                 <div className={classes.usersImg}>
-                    <img src={u.avatar} alt=""/>
+                    <img src={u.photos.small != null ? u.photos.small : userPhoto} alt=""/>
                 </div>
                 <div>
                     {u.followed
@@ -77,10 +29,9 @@ const UsersFunction = (props) => {
                 <span>
                     <div>{u.name}</div>
                     <div>{u.status}</div>
+                    <div>{u.id}</div>
                 </span>
                 <span>
-                    <div>{u.location.city}</div>
-                    <div>{u.location.country}</div>
                 </span>
             </span>
         </div>)}
--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -3,7 +3,56 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let initialState = {
-
+    users: [
+        // {
+        //     id: 1,
+        //     followed: true,
+        //     status: 'I`m boss',
+        //     location: {
+        //         city: 'Dnipro',
+        //         country: 'Ukraine'
+        //     },
+        //     message: 'The message on the card said \'Be my Valentine\'.',
+        //     name: 'User1',
+        //     avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYs5pjJL91GmWCY320ehmbGjILJt5ofZCmxA&usqp=CAU'
+        // },
+        // {
+        //     id: 2,
+        //     followed: false,
+        //     status: 'I`m boss1',
+        //     location: {
+        //         city: 'Dnipro1',
+        //         country: 'Ukraine'
+        //     },
+        //     message: 'She wasn\'t in so I left a message on her answerphone',
+        //     name: 'User2',
+        //     avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6_yv0U_2N_0KokOGogPD0al8ZsxrOK4Hu3A&usqp=CAU'
+        // },
+        // {
+        //     id: 3,
+        //     followed: true,
+        //     status: 'I`m boss2',
+        //     location: {
+        //         city: 'Dnipro2',
+        //         country: 'Ukraine'
+        //     },
+        //     message: 'Can you give Jo a message for me? Tell her I\'ve booked a tennis court for 6.30.',
+        //     name: 'User3',
+        //     avatar: 'https://chto-takoe-lyubov.net/wp-content/uploads/2020/05/Risunki-dlya-srisovki-karandashom-Smeshariki-1.jpg'
+        // },
+        // {
+        //     id: 4,
+        //     followed: false,
+        //     status: 'I`m boss3',
+        //     location: {
+        //         city: 'Dnipro3',
+        //         country: 'Ukraine'
+        //     },
+        //     message: 'When I got home, I found a mysterious message sellotaped to the front door',
+        //     name: 'User4',
+        //     avatar: 'https://klike.net/uploads/posts/2018-06/1528718816_2.jpg'
+        // },
+    ]
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -30,7 +79,7 @@ const usersReducer = (state = initialState, action) => {
                 })
             }
         case SET_USERS: {
-            return {...state, users: [...action.users, ...action.users ]}
+            return {...state, users: [...action.users, ...action.users]}
         }
 
         default:
